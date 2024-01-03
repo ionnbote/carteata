bf2cd1073a1f025180740fb15923d45a0adde768
svetacihan <cihansvetlana@gmail.com>
2024-01-03 13:27:07 +0200

    Removed lab classes

--- src/main/java/Labworks/poo/lab4/InstitutieDeInvatamint.java
+++
@@ -1,8 +0,0 @@
-package Labworks.poo.lab4;
-
-public abstract class InstitutieDeInvatamint {
-
-    public String descriere() {
-        return "O descriere de baza";
-    }
-}
--- src/main/java/Labworks/poo/lab4/InstitutieDeInvatamintMedie.java
+++
@@ -1,8 +0,0 @@
-package Labworks.poo.lab4;
-
-public class InstitutieDeInvatamintMedie extends InstitutieDeInvatamint {
-    @Override
-    public String descriere() {
-        return "O descriere a institutiei de invatamint mediu";
-    }
-}
